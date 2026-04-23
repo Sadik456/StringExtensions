27829424b7741c57640e7a59c8f141ceae159721
Sadik Elahi <selahi5@my.centennialcollege.ca>
2026-04-23 17:33:09 -0400

    Initial commit: StringExtensions class library

---
+++ StringExtensions/StringExtensions/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensions;
+
+public static class StringHelper
+{
+    public static bool StartsWithUpperCase(this string value)
+    {
+        if (string.IsNullOrEmpty(value)) return false;
+        return char.IsUpper(value[0]);
+    }
+}
+
